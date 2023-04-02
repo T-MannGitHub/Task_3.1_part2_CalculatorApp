454c5664ec4e33d9fd31c3ffec2029bf0a0b6ec2
T-MannGitHub <tiff.mann@gmail.com>
2023-04-02 13:48:55 +0930

    Try Catch exception handling added

--- app/src/main/java/com/tmannapps/calculatorapp/MainActivity.java
+++ app/src/main/java/com/tmannapps/calculatorapp/MainActivity.java
@@ -2,13 +2,14 @@ package com.tmannapps.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,13 +20,15 @@ public class MainActivity extends AppCompatActivity {
     Double input1Double, input2Double;
     Double result;
 
-
 // Tiffany Mann SIT708 Task 3.1 Calculator Student ID: 221457972
+//source that assisted my understand the double type manipulation: https://technobyte.org/performing-arithmetic-operations-android-studio/
+
     public void getDouble() {
         input1Double = Double.parseDouble(myInputValue1.getText().toString());
         input2Double = Double.parseDouble(myInputValue2.getText().toString());
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,21 +39,24 @@ public class MainActivity extends AppCompatActivity {
         myAdd = findViewById(R.id.myButtonAdd);
         mySubtract = findViewById(R.id.myButtonSubtract);
 
-        myAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        //suggested from warnings to replace with lambda. Need to review lambda expressions.
+        myAdd.setOnClickListener(view -> {
+            try {
                 getDouble();
                 result = input1Double+input2Double;
                 myResult.setText(Double.toString(result));
+            } catch (Exception exception){
+                Toast.makeText(MainActivity.this, "Please input a number", Toast.LENGTH_SHORT).show();
             }
         });
 
-        mySubtract.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        mySubtract.setOnClickListener(view -> {
+            try {
                 getDouble();
-                result = input1Double-input2Double;
+                result = input1Double - input2Double;
                 myResult.setText(Double.toString(result));
+            } catch (Exception exception) {
+                Toast.makeText(MainActivity.this, "Please input a number", Toast.LENGTH_SHORT).show();
             }
         });
     }
